6f1fabb1b24d106733f2c4d4ab51572d3c457f50
Krishna-Rani-t <105223454+Krishna-Rani-t@users.noreply.github.com>
2022-06-15 17:36:19 +0530

    Update color_pair.h

--- color_pair.h
+++ color_pair.h
@@ -14,13 +14,7 @@ namespace TelCoColorCoder
         "Blue", "Orange", "Green", "Brown", "Slate"};
     int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
     int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major * numberOfMinorColors + minor + 1;}
-    ColorPair GetColorFromPairNumber(int pairNumber) {
-    int zeroBasedPairNumber = pairNumber - 1;
-    MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return ColorPair(majorColor, minorColor);
-    }
+        return major * numberOfMinorColors + minor + 1; }
     class ColorPair {
         private:
             MajorColor majorColor;
@@ -43,7 +37,12 @@ namespace TelCoColorCoder
                return colorPairStr;
             }
        };
-       ColorPair GetColorFromPairNumber(int pairNumber);
+       ColorPair GetColorFromPairNumber(int pairNumber) {
+       int zeroBasedPairNumber = pairNumber - 1;
+       MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+       MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+       return ColorPair(majorColor, minorColor);
+      }
        void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor);
        void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber);
 }
