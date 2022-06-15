65cb6b0de65055ce2ab318147be0483af090bd1e
Krishna-Rani-t <105223454+Krishna-Rani-t@users.noreply.github.com>
2022-06-15 09:04:52 +0530

    Update color_pair.h

--- color_pair.h
+++ color_pair.h
@@ -41,6 +41,8 @@ namespace TelCoColorCoder
                return colorPairStr;
             }
             ColorPair GetColorFromPairNumber(int pairNumber) ;
+            void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor);
+            void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber);
        };
 }
 #endif
