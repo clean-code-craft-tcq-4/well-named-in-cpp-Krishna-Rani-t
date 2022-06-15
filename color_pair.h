c39b29b65a1ffdc29e0aedea9a830c0a2e6ecbbc
Krishna-Rani-t <105223454+Krishna-Rani-t@users.noreply.github.com>
2022-06-15 08:20:41 +0530

    Update color_pair.h

--- color_pair.h
+++ color_pair.h
@@ -2,7 +2,7 @@
 #include <assert.h>
 
 #indef COLOR_PAIR_H
-#def COLOR_PAIR_H
+#define COLOR_PAIR_H
 namespace TelCoColorCoder
 {
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
@@ -11,34 +11,49 @@ namespace TelCoColorCoder
     const char* MajorColorNames[] = {
         "White", "Red", "Black", "Yellow", "Violet"
     };
-    int numberOfMajorColors =
-        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    
+    int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    
     const char* MinorColorNames[] = {
         "Blue", "Orange", "Green", "Brown", "Slate"
     };
-    int numberOfMinorColors =
-        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+    
+    int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
     class ColorPair {
         private:
             MajorColor majorColor;
             MinorColor minorColor;
         public:
-            ColorPair(MajorColor major, MinorColor minor):
-                majorColor(major), minorColor(minor)
-            {}
+            ColorPair(MajorColor major, MinorColor minor):majorColor(major), minorColor(minor)
+            {
+            }
+            
             MajorColor getMajor() {
                 return majorColor;
             }
+            
             MinorColor getMinor() {
                 return minorColor;
             }
+               
             std::string ToString() {
                 std::string colorPairStr = MajorColorNames[majorColor];
                 colorPairStr += " ";
                 colorPairStr += MinorColorNames[minorColor];
                 return colorPairStr;
             }
-    };
-  
-  #endif
+            ColorPair GetColorFromPairNumber(int pairNumber) 
+            {
+                int zeroBasedPairNumber = pairNumber - 1;
+                MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+                MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+                return ColorPair(majorColor, minorColor);
+            }
+            int GetPairNumberFromColor(MajorColor major, MinorColor minor) 
+            {
+                return major * numberOfMinorColors + minor + 1;
+            }
+       }
+};
+#endif
